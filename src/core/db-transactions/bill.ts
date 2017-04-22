ea345bc29193f754aebe75654a53605d8344d38f
AngelJSD <angel.sucapuca@ucsp.edu.pe>
2017-04-22 00:54:22 -0500

    registerBill transaction added

---
+++ src/core/db-transactions/bill.ts
@@ -0,0 +1,24 @@
+import * as Q from 'q';
+import * as dbModels from '../db-models/models';
+import { MongoModel } from '../classes/MongoModel';
+import { Error } from '../interface/Error'
+import { returnServerError } from '../../core/services/returnServerError'
+
+export function registerBill( billData: dbModels.Bill) {
+	let bill: MongoModel = new MongoModel('bill');
+	let deferred = Q.defer();
+
+	let baseBillData: dbModels.Bill = {
+		date : billData.date,
+        subtotal : billData.subtotal,
+        iva : billData.iva,
+        total : billData.total
+	};
+
+	bill.insertOne(baseBillData).then( ( respBillData: dbModels.Bill ) => {
+		deferred.resolve(respBillData);
+	}).catch( () => {
+		returnServerError(deferred)
+	});
+	return deferred.promise;
+}
--- src/server/routers/api.ts
+++ src/server/routers/api.ts
@@ -7,6 +7,7 @@ import { MongoModel } from '../../core/classes/MongoModel';
 import { config } from '../../settings/index';
 import * as dbModels from '../../core/db-models/models'
 import * as Order from '../../core/db-transactions/order';
+import * as Bill from '../../core/db-transactions/bill';
 
 export let apiRoutes: ExpressRouter;
 
@@ -14,6 +15,18 @@ apiRoutes = new ExpressRouter();
 
 apiRoutes.addRoute('POST', '/add/order', (req, res) => {
     Order.registerOrder(req.body).then( (data) => {
+		res.status(200);
+		res.send(data);
+		res.end();
+	}).catch( (err) => {
+		res.status(err.httpStatus);
+		res.send(err.description);
+		res.end();
+	});
+});
+
+apiRoutes.addRoute('POST', '/add/bill', (req, res) => {
+    Bill.registerBill(req.body).then( (data) => {
 		res.status(200);
 		res.send(data);
 		res.end();
