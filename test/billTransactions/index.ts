89506ef1f3939811e4c72c25f32c61b154029f3e
blasdch18 <blas.cruz@ucsp.edu.pe>
2017-05-03 11:51:41 -0500

    test values done

--- test/billTransactions/index.ts
+++ test/billTransactions/index.ts
@@ -5,17 +5,17 @@ import 'mocha';
 
 describe('Register Bill', function () {
 
-  it('Should return ---', (done) => {
+  it('Should return 202', (done) => {
     
     request.post('http://localhost:8000/api/add/bill', {
         form: {
-        date :
-        subtotal :
-        iva :
-        total :
+          date :"actualdate",
+          subtotal :0.00000,
+          iva :0.18,
+          total :0.00000
         }
     }, (err, httpResponse, body) => {
-      assert.equal(  wowwww! =D  , httpResponse.statusCode);
+      assert.equal(  202  , httpResponse.statusCode);
       done();
     
     });
