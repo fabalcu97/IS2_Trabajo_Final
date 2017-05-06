8cfcd4071a1ecb8023e0e54b1dce3c8c755182f2
fabalcu97 <fballoncuadros@gmail.com>
2017-05-05 23:28:45 -0500

    Modified rowData

--- src/server/webapps/register-order/src/app/components/Demo/index.ts
+++ src/server/webapps/register-order/src/app/components/Demo/index.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {UIRouter} from 'ui-router-ng2';
 import { FormGroup,FormArray,FormBuilder,Validators } from '@angular/forms';
 
 
@@ -10,32 +9,28 @@ import { FormGroup,FormArray,FormBuilder,Validators } from '@angular/forms';
 export class DemoComponent implements OnInit {
 
     // Attributes
-        router: UIRouter;
-        rowData = [{
-            nameProduct: '',
-            lots: 'some@one.com',
-            unitsPerLot: '15'
-        }];
+      rowData: any[];
 
-        constructor () {
-            console.log('myTable constructed');
-        }
-  
-    addRow() {
-        this.rowData.push({
-        nameProduct: '',
-        lots: 'Anot@th.er',
-        unitsPerLot: 'AQUI VA LA COnSULTA A LA BS' 
-        })
-    }
-
-    deleteRow(){
-        this.rowData.pop()
-    }
-    
-    ngOnInit() {        
-        
-    }    
-
+    // Methods
+      constructor () {
+         console.log('myTable constructed');
+         this.rowData = [];
+      }
    
+      addRow() {
+         this.rowData.push({
+               nameProduct: '',
+               lots: 'Anot@th.er',
+               unitsPerLot: 'AQUI VA LA COnSULTA A LA BS'
+         });
+      }
+
+      deleteRow(){
+         this.rowData.pop()
+      }
+      
+      ngOnInit() {
+
+      }
+
 }
