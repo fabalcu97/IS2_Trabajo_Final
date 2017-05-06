30c93b85aa151a0d17bbbcbe52499a4c06712844
sergiorvs <segio.rolando@gmail.com>
2017-05-06 00:32:42 -0500

    changed idLocation by locationId

--- src/core/db-models/Lot.ts
+++ src/core/db-models/Lot.ts
@@ -3,6 +3,6 @@ import {BaseModel} from './BaseModel';
 export interface Lot extends BaseModel
 {
     classification : string;    //esto es lo que reemplaza a categoria
-    idLocation : string;
+    locationId : string;
     lot : number;   //cantidad de un producto especifico en lote
 }
