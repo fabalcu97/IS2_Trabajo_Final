dd7116ee08ca210a1e72215bbe77885005e4da3a
Xime <ximena.pocco@ucsp.edu.pe>
2017-05-27 04:18:13 -0500

    report view switch case

--- src/server/webapps/report/src/app/components/Demo/index.ts
+++ src/server/webapps/report/src/app/components/Demo/index.ts
@@ -9,12 +9,15 @@ export class DemoComponent implements OnInit {
 
     // Attributes
        // router: UIRouter;
+       private switchIE: boolean = true;
 
     // Methods
         /*constructor (router: UIRouter) {
             //this.router = router;
         }*/
         constructor(){
+            this.switchIE = true;
+            console.log(this.switchIE);
             
         }
 
