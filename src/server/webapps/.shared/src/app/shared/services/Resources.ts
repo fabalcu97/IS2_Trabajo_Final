a499efd0a3cfe8a1926a64a4e158391422369701
AngelJSD <angel.sucapuca@ucsp.edu.pe>
2017-05-29 00:35:20 -0500

    Check resources

--- src/server/webapps/.shared/src/app/shared/services/Resources.ts
+++ src/server/webapps/.shared/src/app/shared/services/Resources.ts
@@ -18,16 +18,69 @@ export class ResourcesService {
     return this.$http.get('http://localhost:8000/api/get/bill/' + billId).map((res: Response) => res.json());
   }
 
+  public registerBill (bill: Interfaces.Bill): Observable<Interfaces.Bill> {
+    return this.$http.post('http://localhost:8000/api/add/bill/', {
+        subtotal : bill.subtotal,
+        iva : bill.iva,
+        total : bill.total
+	  }).map((res: Response) => res.json());
+  }
+
   public getOrder (billId: String): Observable<Interfaces.Order> {
     return this.$http.get('http://localhost:8000/api/get/order/' + billId).map((res: Response) => res.json());
+  }
+
+  public registerOrder (order: Interfaces.Order): Observable<Interfaces.Order> {
+    return this.$http.post('http://localhost:8000/api/add/order/', {
+      billId : order.billId,
+      guideId : order.guideId,
+      bulkControl : order.bulkControl,
+      arrivalDate : order.arrivalDate,
+      received : order.received,
+      late : order.late
+    }).map((res: Response) => res.json());
   }
 
   public getRemisionGuide (remissionGuideId: String): Observable<Interfaces.Order> {
     return this.$http.get('http://localhost:8000/api/get/guide/' + remissionGuideId).map((res: Response) => res.json());
   }
 
+  public registerRemisionGuide (guide: Interfaces.RemissionGuide): Observable<Interfaces.RemissionGuide> {
+    return this.$http.post('http://localhost:8000/api/add/guide/', {
+      reason : guide.reason,
+      addressee : guide.addressee,
+      departureDate : guide.departureDate,
+      arrivalDate : guide.arrivalDate,
+      transportCompany : guide.transportCompany,
+      vehiclePlate : guide.vehiclePlate,
+      totalWeight : guide.totalWeight
+    }).map((res: Response) => res.json());
+  }
+
   public getDetail (billId: String): Observable<Interfaces.Detail> {
     return this.$http.get('http://localhost:8000/api/get/detail/' + billId).map((res: Response) => res.json());
+  }
+
+  public getValidDetail (billId: String): Observable<Interfaces.Detail[]> {
+    return this.$http.get('http://localhost:8000/api/get/validDetail/' + billId).map((res: Response) => res.json());
+  }
+
+  public updateStoredDetail (detailId: string, detailStored: boolean): Observable<Interfaces.Detail> {
+    return this.$http.post('http://localhost:8000/api/updateStored/detail', {
+      detailId: detailId,
+      detailStored: detailStored
+    }).map((res: Response) => res.json());
+  }
+
+  public registerDetail (detail: Interfaces.Detail): Observable<Interfaces.Detail> {
+    return this.$http.post('http://localhost:8000/api/add/detail/', {
+      billId : detail.billId,
+      productId : detail.productId,
+      quantity : detail.quantity,
+      totalPrice : detail.totalPrice,
+      totalWeight : detail.totalWeight,
+      lotQuantity: detail.lotQuantity
+    }).map((res: Response) => res.json());
   }
 
   public getProduct (productId: String): Observable<Interfaces.Detail> {
@@ -58,5 +111,35 @@ export class ResourcesService {
   public getProducts (): Observable<Interfaces.Product[]> {
     return this.$http.get('http://localhost:8000/api/get/products').map((res: Response) => res.json());
   }
+  
+  public getStorageLocation (storageLocationId: String): Observable<Interfaces.StorageLocation> {
+    return this.$http.get('http://localhost:8000/api/get/slocation/' + storageLocationId).map((res: Response) => res.json());
+  }
+
+  public getAvailableLocations (category: String): Observable<Interfaces.StorageLocation[]> {
+    return this.$http.get('http://localhost:8000/api/get/storagelocation/' + category).map((res: Response) => res.json());
+  }
+
+  public updateStorageLocation (storageLocationId: string, available: boolean): Observable<any> {
+    return this.$http.post('http://localhost:8000/api/updateAvailable/storagelocation', {
+      storageLocationId: storageLocationId,
+      available: available
+    }).map((res: Response) => res.json());
+  }
+
+  public addLot (lot: Interfaces.Lot): Observable<Interfaces.Lot> {
+    return this.$http.post('http://localhost:8000/api/add/lot', {
+      Lot: lot
+    }).map((res: Response) => res.json());
+  }
+
+  public getAvailableLots (productId: String): Observable<Interfaces.Lot[]> {
+    return this.$http.get('http://localhost:8000/api/get/lot/' + productId).map((res: Response) => res.json());
+  }
+
+  //addproduct
+  //getproductbyname
+  //updateArrivalDate
+  //storagelocation
   
 }
