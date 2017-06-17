b714a2717c47bd840133646b33d361604755f39f
fabalcu97 <fballoncuadros@gmail.com>
2017-06-16 23:39:26 -0500

    Added logic and redirection when registering a product

--- src/server/webapps/register-product/src/app/components/Product/index.ts
+++ src/server/webapps/register-product/src/app/components/Product/index.ts
@@ -1,18 +1,43 @@
 import {Component, OnInit} from '@angular/core';
-import {UIRouter} from 'ui-router-ng2';
+import { ResourcesService } from "../../shared/services/Resources";
 
 @Component({
-    selector: 'product',
-    styles: [require('./styles.styl').toString()],
-    template: require('./template.pug')(),
+  selector: 'product',
+  styles: [require('./styles.styl').toString()],
+  template: require('./template.pug')(),
 })
 export class ProductComponent implements OnInit {
 
-    // Attributes
+  // Attributes
+  product: any;
+  resources: ResourcesService;
 
-    // Methods
-        constructor () {}
+  // Methods
+  constructor (resources: ResourcesService) {
+    this.resources = resources;
+    this.product = {
+      category: "",
+      name: "",
+      quantityPerLot: "",
+      unitPrice: "",
+      unitWeight: "",
 
-        ngOnInit () {}
+    }
+  }
 
+  ngOnInit () {
+
+  }
+
+  registerProduct() {
+    this.resources.registerProduct(this.product).subscribe(
+      (data) => {
+        alert("Product registered");
+        window.location = "http://localhost:8000";
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+  }
 }
