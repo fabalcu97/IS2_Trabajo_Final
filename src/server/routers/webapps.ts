1a69eaed44f4c9626e23827e3b9b36e34b89a885
fabalcu97 <fballoncuadros@gmail.com>
2017-07-08 05:02:04 -0500

    Added session validation

--- src/server/routers/webapps.ts
+++ src/server/routers/webapps.ts
@@ -2,6 +2,7 @@ import { ExpressRouter } from '../../core/classes/ExpressRouter';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as express from 'express';
+import * as session from 'express-session';
 import { config } from '../../settings/index';
 
 export let webappsRoutes: ExpressRouter;
@@ -12,7 +13,8 @@ let webappsDir = '../src/server/webapps/';
 let webappsEntries = fs.readdirSync(path.join(__dirname, webappsDir));
 webappsEntries = webappsEntries.filter(function (entry) {
 	let ignore = [
-		'.seed'
+		'.seed',
+		'access'
 	];
 	for (let i = 0; i < ignore.length; i++) {
 		if (ignore[i] == entry) return;
@@ -20,14 +22,28 @@ webappsEntries = webappsEntries.filter(function (entry) {
 	return entry;
 });
 
+webappsRoutes.router.use('/app', (req, res, next) => {
+	if(!req.session) {
+		res.redirect('/access');
+		res.end();
+	}
+	else{
+		next();
+	}
+})
 
 webappsRoutes.addRoute('GET', '/', (req, res) => {
-	res.sendFile(path.join(__dirname, webappsDir, 'home', 'src/index.html'));
+	res.redirect('/app/home');
+	res.end();
 });
 
+webappsRoutes.router.use('/access', express.static(path.join(__dirname, webappsDir, 'access', 'src/dist')));
+webappsRoutes.addRoute('GET', '/access', (req, res) => {
+	res.sendFile(path.join(__dirname, webappsDir, 'access', 'src/index.html'));
+});
 
 webappsEntries.forEach( (webapp) => {
-	let tmpRoute: string = '/' + webapp;
+	let tmpRoute: string = '/app/' + webapp;
 
 	webappsRoutes.router.use(tmpRoute, express.static(path.join(__dirname, webappsDir, webapp, 'src/dist')));
 
