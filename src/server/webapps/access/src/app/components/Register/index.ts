b6642a65473f866ecf32263b1e8378c740f14085
fabalcu97 <fballoncuadros@gmail.com>
2017-07-08 06:34:25 -0500

    Added login and register views

--- src/server/webapps/access/src/app/components/Register/index.ts
+++ src/server/webapps/access/src/app/components/Register/index.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {UIRouter} from 'ui-router-ng2';
+import {UIRouter, StateService} from 'ui-router-ng2';
 import { ResourcesService } from "../../shared/services/Resources";
 
 @Component({
@@ -11,17 +11,58 @@ export class RegisterComponent implements OnInit {
 
     // Attributes
         resources: ResourcesService;
+        state: StateService;
         code: string;
+        confPass: string;
+        person: any;
 
     // Methods
-        constructor (resources: ResourcesService) {
+        constructor (resources: ResourcesService, state: StateService) {
             this.resources = resources;
+            this.state = state;
+            this.confPass = "";
             this.code = "";
+            this.person = {
+                name: "",
+                username: "",
+                password: "",
+                email: "",
+                type: ""
+            }
         }
 
-        ngOnInit () {
-            this.code = prompt("Insert the code");
-            console.log(this.code);
+        ngOnInit () {            
+            this.codeValidation();
+            this.resources.validateCode(this.code).subscribe(
+                (resp) => {
+                    this.person.type = resp.type;
+                    setTimeout( () => {}, 500);
+                },
+                (err) => {
+                    window.location.reload();
+                }
+            )
+        }
+
+        codeValidation() {
+            while(this.code === ""){
+                alert("Code required");
+                this.code = prompt("Insert the code");
+            }
+        }
+
+        submit() {
+            if(this.person.password !== this.confPass) {
+                return;
+            }
+            this.resources.registerPerson(this.person).subscribe(
+                (resp) => {
+                    this.state.go('login');
+                },
+                (err) => {
+                    console.error(err);
+                }
+            )
         }
 
 }
